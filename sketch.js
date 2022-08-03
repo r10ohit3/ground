2d91f87fbc071a265f68889f482b33bb55dd5e55
r10ohit3 <102417637+r10ohit3@users.noreply.github.com>
2022-08-03 12:29:24 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+//u can give ground and isstatic rakhana hai to give SAME WIDTH TO RECT AND BODY
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var ground;
+var wedge;
+var angle=60;
+var poly;
+var boxes=[];
+var r=900
+
+function setup() {
+  createCanvas(1000,1000);
+
+  engine = Engine.create();
+  world = engine.world;
+  var option={
+    isStatic:true
+  };
+  ground=Bodies.rectangle(500,300,r,20,option);
+  World.add(world,ground);
+  //box1 = new Box(200,100,50,50);
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+function mousePressed(){
+  boxes.push(
+    
+    
+    new Box(mouseX,mouseY,50,50)
+    
+    
+    )
+}
+
+function draw() 
+{
+  background(51);
+  rect(ground.position.x,ground.position.y,r,10);
+  Engine.update(engine);
+ for(var i=0;i<boxes.length;i++) {
+ boxes[i].show();}
+  
+  
+}
+
